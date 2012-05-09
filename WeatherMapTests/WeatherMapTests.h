2e557265063a5d45fd0c183fc20e6f774714b8bd
Konstantin Tolochko <katol62@gmail.com>
2012-05-09 09:48:42 +0300

    WeatherMapApp tests dir

---
+++ WeatherMapTests/WeatherMapTests.h
@@ -0,0 +1,13 @@
+//
+//  WeatherMapTests.h
+//  WeatherMapTests
+//
+//  Created by apple on 07.05.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WeatherMapTests : SenTestCase
+
+@end
